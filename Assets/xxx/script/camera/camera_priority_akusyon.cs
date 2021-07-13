85f4a3b8b85eb8f6a797577d6e3a82c39e3fc41a
hgoegheo <morimori2003@gmail.dom>
2021-07-13 15:43:05 +0900

    アクション用のカメラワーク作成

---
+++ Assets/xxx/script/camera/camera_priority_akusyon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class camera_priority_akusyon : MonoBehaviour
+{
+    public camera_hantei_script CHS;
+    string camera_hantei;
+
+    public GameObject OBJ;
+
+    CinemachineVirtualCamera CVcamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CVcamera = this.gameObject.GetComponent<CinemachineVirtualCamera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camera_hantei = CHS.camera_hantei;
+
+        if (OBJ.name == camera_hantei)
+        {
+            CVcamera.Priority = 200;
+        }
+        else
+        {
+            CVcamera.Priority = 1;
+        }
+    }
+}
