5c55a5899d84281922d94550bb19430a5bf1ba5f
hgoegheo <morimori2003@gmail.dom>
2021-07-22 09:50:13 +0900

    アクション
    場所によってアクションが発火するようにした

--- Assets/xxx/script/sea/test_sia.cs
+++ Assets/xxx/script/sea/test_sia.cs
@@ -4,35 +4,59 @@ using UnityEngine;
 
 public class test_sia : MonoBehaviour
 {
+    //スクリプトを動かすかどうか(移動)
+    string stop = "GO";
+
     public float run_speed;
     Animator animator;
+    Rigidbody rb;
+    CapsuleCollider CC;
+
+    //アクション情報を取得するための変数
+    public camera_hantei_script CHS;
+    string camera_hantei;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+
+        rb = GetComponent<Rigidbody>();
+        CC = GetComponent<CapsuleCollider>();
     }
 
     void Update()
     {
+        //アクションの情報の取得
+        camera_hantei = CHS.camera_hantei;
+        //入力の取得
         Vector3 target_dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
-        //Run
-        if (target_dir.magnitude > 0.1)
+        //旅人と話をするときのスクリプト
+        if (Input.GetKey(KeyCode.G) && camera_hantei == "cinema_tabibito_hinkon")
         {
-            //体の向きを変更
-            transform.rotation = Quaternion.LookRotation(target_dir);
-            //Quaternion rotation = Quaternion.LookRotation(target_dir);
-            //transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * smooth);
-
-            //アニメーション
-            animator.SetInteger("walk", 1);
-
-            //前方へ移動
-            transform.Translate(Vector3.forward * Time.deltaTime * run_speed);
+            animator.SetInteger("walk", 9);
+            CC.enabled = false;
+            rb.useGravity = false;
+            stop = "STOP";
         }
-        else
+
+        //移動のスクリプト
+        if (stop == "GO")
         {
-            animator.SetInteger("walk", 0);
+            //Run
+            if (target_dir.magnitude > 0.1)
+            {
+                //体の向きを変更
+                transform.rotation = Quaternion.LookRotation(target_dir);
+                //アニメーション
+                animator.SetInteger("walk", 1);
+                //前方へ移動
+                transform.Translate(Vector3.forward * Time.deltaTime * run_speed);
+            }
+            else
+            {
+                animator.SetInteger("walk", 0);
+            }
         }
     }
 }
