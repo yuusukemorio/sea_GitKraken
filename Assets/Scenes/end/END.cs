180ff15a0622cb2cdd5683ff5191ac065d44d975
yusukee <morimori2003@gmail.com>
2021-09-05 08:37:07 +0900

    灯台に合わせて位置を移動させる、

---
+++ Assets/Scenes/end/END.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class END : MonoBehaviour
+{
+    void Start()
+    {
+        SceneManager.LoadScene("end");
+    }
+}
---
+++ Assets/xxx/script/sea/toudai_hairu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toudai_hairu : MonoBehaviour
+{
+    public camera_hantei_script CHS;
+    string camera_hantei;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camera_hantei = CHS.camera_hantei;
+
+        if(camera_hantei == "toudai_deru")
+        {
+            transform.position = new Vector3(transform.position.x, 3.437769f, transform.position.z);
+        }
+        //if (camera_hantei == "toudai_hairu")
+        //{
+        //    transform.position = new Vector3(transform.position.x, 3.934518f, transform.position.z);
+        //}
+    }
+}
